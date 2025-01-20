c71f9cdfb8ef71825cea270dfe8508d80921d8ba
J-JH-01 <winter55555@naver.com>
2025-01-20 09:47:56 +0900

    3

--- react/et-admin-client/src/components/DashBoard.js
+++ react/et-admin-client/src/components/DashBoard.js
@@ -58,9 +58,17 @@ export default function DashBoard() {
     padding: 8px;
     border-radius: 4px;
     transition: background-color 0.2s;
+    width: 35px;
+    height: 35px;
 
     &:hover {
       background-color: #357abd;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      fill: currentColor;
     }
   `;
 
@@ -151,7 +159,9 @@ export default function DashBoard() {
       <Title className="dash-board-title">
         <StyledNavLink to="/">관리자 페이지</StyledNavLink>
         <BackButton href="http://modeunticket.store/">
-          <Home size={20} />
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+          </svg>
         </BackButton>
       </Title>
 
